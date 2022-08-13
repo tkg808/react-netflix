b2b1cbb6ffb797796a821f38092524887a778066
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-08-13 14:54:09 -0700

    Refactor class names

--- src/components/loginForm/LoginForm.js
+++ src/components/loginForm/LoginForm.js
@@ -27,39 +27,39 @@ export default function LoginForm()
         <h2 className="login-form-title">
           Sign In
         </h2>
-        <div className="user-info-container">
+        <div className="login-form-user-contact-input-container">
           <input
             type="text"
-            id="login-form-user-info"
+            id="login-form-user-contact-input"
             value={formData.username}
             onChange={handleChange} />
-          <label htmlFor="login-form-user-info">Email or phone number</label>
+          <label htmlFor="login-form-user-contact-input">Email or phone number</label>
         </div>
-        <div className="user-password-container">
+        <div className="login-form-user-password-input-container">
           <input
             type="text"
-            id="login-form-user-password"
+            id="login-form-user-password-input"
             value={formData.password}
             onChange={handleChange} />
-          <label htmlFor="login-form-user-password">Password</label>
+          <label htmlFor="login-form-user-password-input">Password</label>
         </div>
         <button className="login-form-submit-button">Sign In</button>
         <div className="login-form-main-bottom-row">
           <div className="login-form-checkbox-container">
-            <input type="checkbox" id="login-user-remember" />
-            <label htmlFor="login-user-remember">Remember me</label>
+            <input type="checkbox" id="login-form-user-remember" />
+            <label htmlFor="login-form-user-remember">Remember me</label>
           </div>
           <Link to="" className="login-form-help-link">Need help?</Link>
         </div>
       </form>
       <div className="login-form-other">
-        <div className="login-signup-container">
+        <div className="login-form-signup-container">
           New to Netflix? <Link to="" className="login-form-signup-link">Sign up now.</Link>
         </div>
-        <div className="recaptcha-container">
-          <p className="recaptcha-text">
+        <div className="login-form-recaptcha-container">
+          <p className="login-form-recaptcha-text">
             This page is protected by Google reCAPTCHA to ensure you're not a bot.
-            <Link to="" className="recaptcha-terms-of-use-link"> Learn more.</Link>
+            <Link to="" className="login-form-recaptcha-terms-of-use-link"> Learn more.</Link>
           </p>
         </div>
       </div>
