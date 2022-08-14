8138b34cd44db33321132adf286be3e82670c431
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-08-13 17:11:40 -0700

    Add validation functions for user login

---
+++ src/hooks/UserValidation.js
@@ -0,0 +1,60 @@
+const validateEmail = function (email)
+{
+  const LENGTH = email.length;
+
+  if (LENGTH < 5)
+  {
+    return false;
+  }
+
+  let hasDot = false;
+  let hasAt = false;
+  let i = 0;
+
+  while ((i < LENGTH) && (!hasDot || !hasAt))
+  {
+    if (email[i] === ".")
+    {
+      hasDot = true;
+    }
+
+    if (email[i] === "@")
+    {
+      hasAt = true;
+    }
+
+    i += 1;
+  }
+
+  return (hasDot && hasAt);
+}
+
+const validatePhone = function (phone)
+{
+  const LENGTH = phone.length;
+
+  if (LENGTH !== 10)
+  {
+    return false;
+  }
+
+  for (let i = 0; i < LENGTH; i++)
+  {
+    let curr = phone.charCodeAt(i);
+    if (curr < 48 || 57 < curr)
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const validatePassword = function (password)
+{
+  const LENGTH = password.length;
+
+  return (4 <= LENGTH && LENGTH <= 60);
+}
+
+export { validateEmail, validatePhone, validatePassword };
