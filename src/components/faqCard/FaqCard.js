f3b0ff9c1d66e7d71b4031add7216e325dc4e23b
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-08-28 14:54:13 -0700

    Update FaqCard name structure

--- src/components/faqCard/FaqCard.js
+++ src/components/faqCard/FaqCard.js
@@ -11,28 +11,26 @@ export default function FaqCard()
   const [validFaqInput, setValidFaqInput] = useState(false);
 
   return (
-    <div className="faq-card-container">
-      <div className="faq-card-content">
+    <div className="faq-card">
+      <div className="content-container">
         <h1>Frequently Asked Questions</h1>
-        <ul className="faq-card-list">
+        <ul>
           {
             questionsData.map((item, index) => 
             {
               return (
                 <li
-                  className="faq-card-list-item"
                   key={index}>
-                  <button className="faq-question" onClick={() => currItem === index ? setCurrItem(-1) : setCurrItem(index)}>
+                  <button onClick={() => currItem === index ? setCurrItem(-1) : setCurrItem(index)}>
                     {item.question}
                     <svg
-                      id="question-icon"
                       viewBox="0 0 26 26"
-                      className={"svg-question-icon" + (currItem === index ? " open" : " close")}
+                      className={"question-icon" + (currItem === index ? " open" : " close")}
                       focusable="true">
                       <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z" />
                     </svg>
                   </button>
-                  <div className={"faq-answer" + (currItem === index ? " open" : " close")}>
+                  <div className={"answer" + (currItem === index ? " open" : " close")}>
                     <span>
                       {item.answers[0]}
                       {item.answers[1] && <br></br>}
