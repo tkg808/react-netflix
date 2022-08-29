a3b1b6831f863ffd64320720dd5212d4261db823
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-08-28 19:45:01 -0700

    Add Browse page; Add ProfilesGate and style

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import { UserContext } from './hooks/Contexts';
 import Landing from './pages/landing/Landing';
 import Login from './pages/login/Login';
+import Browse from './pages/browse/Browse';
 
 export default function App()
 {
@@ -15,6 +16,7 @@ export default function App()
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/browse" element={<Browse />} />
         </Routes>
       </UserContext.Provider>
     </div >
