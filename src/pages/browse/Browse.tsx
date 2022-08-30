57a3a6499c6eca4b48494d9f5ae388acdbbd1536
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-08-29 18:59:21 -0700

    Add header and nav to Browse page

--- src/pages/browse/Browse.tsx
+++ src/pages/browse/Browse.tsx
@@ -1,6 +1,9 @@
 import './Browse.css';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import ProfilesGate from '../../components/profilesGate/ProfilesGate';
+import { LogoIcon } from '../../res/LogoIcon';
+import { tabsDataList } from '../../res/tabsDataList';
 
 interface Profile
 {
@@ -15,6 +18,29 @@ export default function Browse()
 
   return (
     <div className="browse-page">
+      <div className="pinned-header-container">
+        <header>
+          <Link to="" className="logo-link">
+            <LogoIcon />
+          </Link>
+          <nav className="tabbed-nav">
+            <ul className="tabs-list">
+              {
+                tabsDataList.map((item, index) =>
+                {
+                  return (
+                    <li key={index}>
+                      <Link to={item.linkPath} tabIndex={index}>
+                        {item.title}
+                      </Link>
+                    </li>
+                  );
+                })
+              }
+            </ul>
+          </nav>
+        </header>
+      </div>
       {!currProfile && <ProfilesGate setCurrProfile={setCurrProfile} />}
     </div >
   );
